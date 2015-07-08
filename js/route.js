963d55e6c371e10722ed6e42ab10007d0b51aecf
Ramakrishna Kollarapu <Ram@Ram-MacBook-Pro.local>
2015-07-08 14:38:34 -0400

    singlePageApp

---
+++ js/route.js
@@ -0,0 +1,45 @@
+    //create a module
+    var myApp = angular.module('myApp', ['ngRoute']);
+
+        //configure routes
+        myApp.config(function($routeProvider) {
+        $routeProvider
+
+            //route the pages
+            //route for homepage
+                .when ('/home', {
+                templateUrl : 'pages/home.html',
+                controller  : 'mainController'
+            })
+
+            //route for aboutpage
+                .when ('/about', {
+                templateUrl : 'pages/about.html',
+                controller  : 'aboutController'
+            })
+
+            //route for servicespage
+                .when ('/services', {
+                templateUrl : 'pages/services.html',
+                controller  : 'servicesController'
+            })
+
+            //route for contactpage
+                .when ('/contact', {
+                templateUrl : 'pages/contact.html',
+                controller  : 'contactController'
+            });
+        });
+
+    //create controller and inject angular
+    myApp.controller('mainController', function() {
+    });
+
+    myApp.controller('aboutController', function() {
+    });
+
+    myApp.controller('servicesController', function() {
+    });
+
+    myApp.controller('contactController', function() {
+    });
